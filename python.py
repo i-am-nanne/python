a458c15da24e69567822bdc562cb61de976abe2f
nanne <semicolon2@protonmail.com>
2019-02-11 19:05:10 +0530

    7.String Formatting

---
+++ python.py
@@ -0,0 +1,11 @@
+num1=3.1234
+num2=42.5678
+num3=102.12314
+#	print("string",VARIABLES,"string")
+print("num1 is",num1,"num2 is",num2,"num3 is",num3)
+
+#	print("string {INDEX}".format(VARIABLES))
+print("num1 is {0} and num2 is {1:.2} and num3 is {2:.2f}".format(num1,num2,num3))
+
+#	print(f"string {VARIABLES}")
+print(f"num1 is {num1} and num2 is {num2:.3} and num3 is {num3:.3f}")
