00b14002ff73fca9f40ca78032d9811e69aea264
nanne <semicolon2@protonmail.com>
2019-02-11 22:12:17 +0530

    11.2 ranges passing two value(start,stop)

--- python.py
+++ python.py
@@ -2,9 +2,9 @@
 #	
 #	range(start,stop,step)
 #
-# 	when u pass only one value to range it consider
-#	it as end point(which is not inclued) and start from 0 
-#	and step 1
+# 	when you pass two value to range it consider first value
+# 	as start point and second value as end point(which is not 
+# 	inclued) and step is 1
 
-for r in range(10):
+for r in range(10,20):
 	print(r)
