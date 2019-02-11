06588f483f8302d5f215acaa0662befc63e37f36
nanne <semicolon2@protonmail.com>
2019-02-11 22:14:27 +0530

    11.3 ranges passing three values (start,stop,step)

--- python.py
+++ python.py
@@ -2,9 +2,9 @@
 #	
 #	range(start,stop,step)
 #
-# 	when you pass two value to range it consider first value
+# 	when you pass three values to range it consider first value
 # 	as start point and second value as end point(which is not 
-# 	inclued) and step is 1
+# 	inclued) and third value as step(increament) 
 
-for r in range(10,20):
+for r in range(10,20,2):
 	print(r)
