a3a7b117d71d2b48312deefb7ceb72a06d786411
nanne <semicolon2@protonmail.com>
2019-02-14 22:50:13 +0530

    19. Modules and Pacakges | creating packages and importing the packages

--- python.py
+++ python.py
@@ -1,7 +1,8 @@
 #************* 19. Modules and Packages **********
 #	Module:	Just a python file
 #	Package : Collection of modules
-#
+#	
+#	Module:
 #	To import the module there are two ways:
 #	1.
 #		import module_name(nothing but filename)
@@ -13,10 +14,22 @@
 #		from module_name import specific_function_name
 #		specific_function_name()
 #
+#	Package:
+#		For creating a package we need to choose a folder then we add a file named
+#	__init__.py file and then we add the python files that are going to be used
+#	as modules
+#	
+#	To import the package
+#	from package_name.module_name import *
+#
 #
 
-from module_example import *
-
+from test.module_example import *
+from test.print_some_stuff import *
 print(add(20,50))
 print(sub(50,20))
 print(mult(20,50))
+print("invoking the the fun function")
+fun()
+print("invoking the getAuthorName function")
+getAuthorName()
---
+++ test/print_some_stuff.py
@@ -0,0 +1,5 @@
+def fun():
+	print("Sorry ! there is nothing special in the function ")
+
+def getAuthorName():
+	print("The author of code is Nanne ! :)")
