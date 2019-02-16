c45d68a0255df25552c37b1f41a8383162cf8fb1
nanne <semicolon2@protonmail.com>
2019-02-16 22:07:21 +0530

    23. Filters | Normal way to find even numbers

--- python.py
+++ python.py
@@ -1,34 +1,13 @@
-#********** 22. MAPS ********
-#		Maps are way to take a list apply some kind of function
-#	to each item in the list to change the items and splitout
-#	new list from these updated items within it
-#
-#	map(function,data)
-#
+#********** 23. FILTERS ********
+#		Filters gives us an easy way to take a collection
+#	and filter out some other values based on function result
+#	this gives us filter colletion
 
-#square the items in a list using maps
+# Normal way to find the even numbers in a list
 
-def square(num):
-	return num*num
-
-nums=[1,3,5,7,9]
-print(list(map(square,nums)))
-
-#comprehension way to square the items in a list
-
-#def square(num):
-#	return num*num
-#nums=[1,2,3,4,5]
-#square_nums=[square(num) for num in nums]
-#print(square_nums)
-
-
-#Normal way to square the items in a list
-
-#def square(num):
-#	return num*num
-#nums=[1,2,3,4,5,6,7,8,9]
-#square_nums=[]
-#for num in nums:
-#	square_nums.append(square(num))
-#print(square_nums)
+nums=[1,2,3,4,5,6,7,8,9,10]
+evenNums=[]
+for num in nums:
+	if(num%2==0):
+		evenNums.append(num)
+print(evenNums)
