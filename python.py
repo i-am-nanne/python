6b5a0b6101bfde593a46b65f3296c101da520d1e
nanne <semicolon2@protonmail.com>
2019-02-16 22:15:22 +0530

    23. Filters | finding even numbers using filters

--- python.py
+++ python.py
@@ -3,11 +3,23 @@
 	and filter out some other values based on function result
 	this gives us filter colletion
 """
+# finding the even numbers using filters
+
+def even_num(num):
+	return num%2==0		# return true or false 
+
+nums=[1,2,3,4,5,6,7,8,9,10]
+print(list(filter(even_num,nums)))
+
+
+
+"""
 #comprehension way to find the even numbers
+
 nums=[1,2,3,4,5,6,7,8,9,10]
 evenNums=[num for num in nums if(num%2==0)]
 print(evenNums)
-
+"""
 
 """ 
 #	Normal way to find the even numbers in a list
