a16f9e3434c32f9687def5a30ab00d4d8bc595c7
nanne <semicolon2@protonmail.com>
2019-02-16 18:41:49 +0530

    22. Maps |comprehension way to square the items in the list

--- python.py
+++ python.py
@@ -6,14 +6,23 @@
 #	map(function,data)
 #
 
-#Normal way to square the items in a list
-
+#comprehension way to square the items in a list
 def square(num):
 	return num*num
 
-nums=[1,2,3,4,5,6,7,8,9]
-square_nums=[]
-for num in nums:
-	square_nums.append(square(num))
-
+nums=[1,2,3,4,5]
+square_nums=[square(num) for num in nums]
 print(square_nums)
+
+
+#Normal way to square the items in a list
+
+#def square(num):
+#	return num*num
+
+#nums=[1,2,3,4,5,6,7,8,9]
+#square_nums=[]
+#for num in nums:
+#	square_nums.append(square(num))
+
+#print(square_nums)
