a704ba8daaa6321a26d92fe80a93a82edb4765de
nanne <semicolon2@protonmail.com>
2019-02-17 22:49:59 +0530

    26. Reading Files | Method 4

--- python.py
+++ python.py
@@ -1,7 +1,3 @@
 #********** 26. Reading files ********
-
-file=open("text.txt")
-file.seek(0)
-#read takes numbers of chareters to read
-print(file.read(10))
-file.close()
+with open("text.txt") as file:
+	print(file.readlines())
