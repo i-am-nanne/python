d565793458617fb6ed9e525c4b68202bf6a8fab6
nanne <semicolon2@protonmail.com>
2019-02-17 22:39:07 +0530

    26. Reading Files | Method 1

--- python.py
+++ python.py
@@ -1,33 +1,7 @@
-"""********** 25. DECORATORS ********
-	Decorators basically extends the behaviour of functions,that
-ussued on without modifying the function itself
-	
-	def decorator_name(function):
-		def wrapper_function():
-			//code to execute before the function
-			function()
-			//code to execute after the function
-		return wrapper_function
-	
-	@decorator_name
-	def function():
-		//code
+#********** 26. Reading files ********
 
-	@decorator_name
-"""
+text_file=open("text.txt")
+for line in text_file:
+	print(line.rstrip())
 
-def wish(s):
-	def wrapper():
-		print("welcome to the city")
-		s()
-		print("Thank you for visiting ! visit again")
-
-	return wrapper()
-
-@wish
-def jmd_city():
-	print("This is jmd city")
-
-@wish
-def pdtr_city():
-	print("This is pdtr city")
+text_file.close()
